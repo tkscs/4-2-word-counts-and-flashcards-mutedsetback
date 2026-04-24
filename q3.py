6e3e3ec56f6d6ccafd0a84385c6e4b872ad3f870
mutedsetback <bcrawford2028@kehillahstudent.org>
2026-04-24 11:41:59 -0700

    commit

--- q3.py
+++ q3.py
@@ -2,19 +2,25 @@
 flashcards = {
     "question": "answer"
 }
+import random
 
 # Get a list of keys (questions) from the dictionary
 #### YOUR CODE HERE
-
+questions = list(flashcards.keys())
 # Randomly sample one question
 #### YOUR CODE HERE
+question = random.choice(questions)
 
 # Use the `input` function to ask the user the question and get their response
 #### YOUR CODE HERE
+response = input(question + " ")
 
 # Use the question as a key to look up the answer in the dicitonary
 #### YOUR CODE HERE
+answer = flashcards[question]
 
 # Check if the response is the same as the answer, and give the user
 # feedback based on whether their response was correct or incorrect
 #### YOUR CODE HERE
+if response == answer:
+    print("correct")
